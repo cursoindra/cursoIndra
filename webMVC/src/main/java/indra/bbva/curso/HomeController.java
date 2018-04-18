843189f7f186020b02306685b5d518d0355f3227
cursoindra <37654708+cursoindra@users.noreply.github.com>
2018-04-18 17:39:04 +0200

    controlador sin agenda

--- webMVC/src/main/java/indra/bbva/curso/HomeController.java
+++ webMVC/src/main/java/indra/bbva/curso/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import indra.bbva.curso.datos.Agenda;
+
 
 /**
  * Handles requests for the application home page.
@@ -42,48 +42,6 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping("/datos")
-	public List<Agenda> datos() {
-		List<Agenda> datos = new ArrayList<Agenda>();
-		Agenda agenda1= new Agenda();
-		agenda1.setNombre("Javier");
-		agenda1.setDireccion("mi casa");
-		agenda1.setTelefono("9199999999");
-		datos.add(agenda1);
-		
-		Agenda agenda2= new Agenda();
-		agenda2.setNombre("Javier1");
-		agenda2.setDireccion("mi casa1");
-		agenda2.setTelefono("91999999991");
-		datos.add(agenda2);
-		
-		Agenda agenda3= new Agenda();
-		agenda3.setNombre("Javier2");
-		agenda3.setDireccion("mi casa2");
-		agenda3.setTelefono("91999999992");
-		datos.add(agenda3);
-		
-		Agenda agenda4= new Agenda();
-		agenda4.setNombre("Javier3");
-		agenda4.setDireccion("mi casa3");
-		agenda4.setTelefono("91999999993");
-		datos.add(agenda4);
-		
-		Agenda agenda5= new Agenda();
-		agenda5.setNombre("Javier4");
-		agenda5.setDireccion("mi casa4");
-		agenda5.setTelefono("91999999994");
-		datos.add(agenda5);
-		
-		Agenda agenda6= new Agenda();
-		agenda6.setNombre("Javier5");
-		agenda6.setDireccion("mi casa5");
-		agenda6.setTelefono("91999999995");
-		datos.add(agenda6);
-		
-		
-		
-		return datos;
-	}
+	
 
 }
