65a14364a7ca30cafd0677a401d1332d0dced19b
ztitus <33697673+ztitus@users.noreply.github.com>
2018-04-18 13:13:27 +0200

    pp

---
+++ cursoJavascript/WebContent/js/Publicaciones.js
@@ -0,0 +1,22 @@
+//datos personales,cal,mensa,eventos
+
+class Publicaciones{
+
+	constructor(id,titulo,contenido,url){
+		this.id=id;
+		this.titulo=titulo;
+		this.contenido=contenido;
+		this.url=url;
+
+	}
+}
+class Comentarios{
+
+
+	constructor(id,texto)
+	{
+		this.id=id;
+		this.texto=texto;
+
+	}
+}
