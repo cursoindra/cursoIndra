858432dec7831966d10877e720f1883f767b9592
Durán Martín <fjdbastante@eservicios.indra.es>
2018-04-23 15:57:36 +0200

    mejora de login

---
+++ cursoJavascript/WebContent/js/login.js
@@ -0,0 +1,23 @@
+class Login{
+	constructor(email, pass){
+	this.user = email;
+	this.pass = pass;
+	}
+}
+
+function entrar(){
+	var user = document.getElementById('email').value;
+	var pass = document.getElementById('pass').value;
+
+	var login = new Login();
+	login.user = user;
+	login.pass = pass;
+
+	var datoJson = JSON.stringify(login);
+	alert(datoJson);
+}
+
+function recuperarPass(){
+	var email = document.getElementById('email2').value;
+	alert(email);
+}
