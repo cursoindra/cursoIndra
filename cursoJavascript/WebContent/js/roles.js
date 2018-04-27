9b99bfe74a403d53865d4b59571b3fbfeebaad63
cursoindra <fjdbastante@eservicios.indra.es>
2018-04-27 11:08:47 +0200

    last-AltaNoticias

--- cursoJavascript/WebContent/js/roles.js
+++ cursoJavascript/WebContent/js/roles.js
@@ -6,3 +6,7 @@ class Rol{
 		this.rol=rol;
 	}
 }
+
+//Example roles
+var roles = new Array({ "rol": "contractors", "id": 6 }, { "rol": "velociraptors", "id": 7 }, { "rol": "predators", "id": 7 }, { "rol": "tractors", "id": 8 });
+export {roles}
