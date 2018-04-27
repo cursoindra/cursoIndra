6ae37439afb077299020b99800f8eaa8e6a3e7e3
cursoindra <37654708+cursoindra@users.noreply.github.com>
2018-04-27 11:09:19 +0200

    hey noticea esto que lo he actualizado

--- cursoJavascript/WebContent/js/Publicaciones.js
+++ cursoJavascript/WebContent/js/Publicaciones.js
@@ -2,16 +2,8 @@ var templateNews = document.getElementById("news-template").content;
 var templateComments = document.getElementById("comments-template").content;
 var templateNewComment = document.getElementById("new-comment-template").content;
 
-if (localStorage.getItem("publicaciones") == null)
-    var publicaciones = new Array();
-else
-    var publicaciones = JSON.parse(localStorage.getItem("publicaciones"));
-
-if (localStorage.getItem("comentarios") == null)
-    var comentarios = new Array();
-else
-    var comentarios = JSON.parse(localStorage.getItem("comentarios"));
-
+var publicaciones;
+var comentarios;
 //var usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
 
@@ -29,82 +21,117 @@ class Comentario {
 	}
 }
 
+function reloadPublish () {
 
-publicaciones.forEach((publicacion, index) => {
-	
-	var noticia = templateNews.cloneNode(true);
-	noticia.getElementById("news-title").textContent = publicacion.titulo;
-	noticia.getElementById("news-content").textContent = publicacion.contenido;
-	noticia.getElementById("like-img").setAttribute("src", "images/like.png");
-	noticia.querySelector(".like-button").setAttribute("href", index);
-	noticia.getElementById("collapse-button").setAttribute("data-target", "#collapse"+index);
-	noticia.getElementById("collapse").setAttribute("id", "collapse"+index);
-	noticia.getElementById("num-comments").textContent = publicacion.comentarios.length;
-	publicacion.comentarios.forEach(comentario => {
-		var comentario = comentarios.filter((com)=>{
-			return com.id == comentario;
-		})[0];
-		if (comentario) {
-			/*
-			var user = usuarios.filter((us) => {
-				return us.nombre == comentario.propietario;
-			}); 
-			*/
-			var com = templateComments.cloneNode(true);
-			com.getElementById("user-avatar").setAttribute("src", "images/img_avatar1.png"); // TODO: mostrar img del user
-			//com.getElementById("user-name").textContent = user.nombre;
-			com.getElementById("user-name").textContent = "user.nombre";
-			com.getElementById("comment-content").textContent = comentario.texto;
+	loadVars();
+	cleanPublish();
 
-			noticia.getElementById("comments").appendChild(com);
-		}
-	});
+	publicaciones.forEach((publicacion, index) => {
 
-	var newCom = templateNewComment.cloneNode(true);
-	newCom.getElementById("numNoticia").value = index;
-	noticia.getElementById("comments").appendChild(newCom);
+		var noticia = templateNews.cloneNode(true);
+		noticia.getElementById("news-title").textContent = publicacion.titulo;
+		noticia.getElementById("news-content").textContent = publicacion.contenido;
+		noticia.getElementById("like-img").setAttribute("src", "images/like.png");
+		noticia.querySelector(".like-button").setAttribute("href", index);
+		noticia.getElementById("collapse-button").setAttribute("data-target", "#collapse" + index);
+		noticia.getElementById("collapse").setAttribute("id", "collapse" + index);
+		noticia.getElementById("num-comments").textContent = publicacion.comentarios.length;
+		publicacion.comentarios.forEach(comentario => {
+			var comentario = comentarios.filter((com) => {
+				return com.id == comentario;
+			})[0];
+			if (comentario) {
+				/*
+				var user = usuarios.filter((us) => {
+					return us.nombre == comentario.propietario;
+				}); 
+				*/
+				var com = templateComments.cloneNode(true);
+				com.getElementById("user-avatar").setAttribute("src", "images/img_avatar1.png"); // TODO: mostrar img del user
+				//com.getElementById("user-name").textContent = user.nombre;
+				com.getElementById("user-name").textContent = "user.nombre";
+				com.getElementById("comment-content").textContent = comentario.texto;
 
-	document.getElementById("news").appendChild(noticia);
-});
-
-if(publicaciones.length){
-
-	[...document.getElementsByClassName("form_comentario")].forEach((form)=>{
-
-		form.addEventListener("submit", function(event){		
-			var numNoticia = form.querySelector("#numNoticia").value,
-				texto = form.querySelector("#textoComentario").value;
-
-			var com = new Comentario(comentarios.length, texto, "Pepe", []);
-
-			comentarios.push(com);
-
-			// añadir el comentario a la noticia
-
-			publicaciones[numNoticia].comentarios.push(com.id);
-
-			// resubir las noticias y el comentario
-
-			localStorage.setItem("publicaciones", JSON.stringify(publicaciones));
-			localStorage.setItem("comentarios", JSON.stringify(comentarios));
-
-			location.reload();
+				noticia.getElementById("comments").appendChild(com);
+			}
 		});
+
+		var newCom = templateNewComment.cloneNode(true);
+		newCom.getElementById("numNoticia").value = index;
+		noticia.getElementById("comments").appendChild(newCom);
+
+		document.getElementById("news").appendChild(noticia);
 	});
 
-	[...document.getElementsByClassName("like-button")].forEach((button)=>{
+	if (publicaciones.length) {
 
-		button.addEventListener("click", function(event){
-			event.preventDefault();	
+		[...document.getElementsByClassName("form_comentario")].forEach((form) => {
 
-			var numNoticia = button.getAttribute("href");
+			form.addEventListener("submit", function (event) {
+				var numNoticia = form.querySelector("#numNoticia").value,
+					texto = form.querySelector("#textoComentario").value;
 
-			publicaciones[numNoticia].likes.push("Pepe");
+				var com = new Comentario(comentarios.length, texto, "Pepe", []);
 
-			// resubir las noticias y el comentario
+				comentarios.push(com);
 
-			localStorage.setItem("publicaciones", JSON.stringify(publicaciones));
+				// añadir el comentario a la noticia
+
+				publicaciones[numNoticia].comentarios.push(com.id);
+
+				// resubir las publicaciones y el comentario
+
+				localStorage.setItem("publicaciones", JSON.stringify(publicaciones));
+				localStorage.setItem("comentarios", JSON.stringify(comentarios));
+
+				location.reload();
+			});
 		});
-	});
+
+		[...document.getElementsByClassName("like-button")].forEach((button) => {
+
+			button.addEventListener("click", function (event) {
+				event.preventDefault();
+
+				var numNoticia = button.getAttribute("href");
+
+				publicaciones[numNoticia].likes.push("Pepe");
+
+				// resubir las publicaciones
+
+				localStorage.setItem("publicaciones", JSON.stringify(publicaciones));
+			});
+		});
+
+	}
 
 }
+
+function cleanPublish () {
+	var nodoPubli = document.getElementById("news");
+	while (nodoPubli.firstChild) {
+		nodoPubli.removeChild(nodoPubli.firstChild);
+	}
+}
+
+function loadVars () {
+	if (localStorage.getItem("publicaciones") == null)
+		publicaciones = new Array();
+	else
+		publicaciones = JSON.parse(localStorage.getItem("publicaciones"));
+
+	if (localStorage.getItem("comentarios") == null)
+		comentarios = new Array();
+	else
+		comentarios = JSON.parse(localStorage.getItem("comentarios"));
+}
+
+function loadComments () {
+
+}
+
+reloadPublish();
+
+setInterval(()=>{
+	reloadPublish();
+},15000);
